bc60e77e28446152c04878463a0af1a49a143790
Tim <timofeyobraztsov@gmail.com>
2023-06-23 00:00:00 -0700

    website

--- build/server.js
+++ build/server.js
@@ -10,15 +10,34 @@ const userroute_1 = __importDefault(require("./routes/userroute"));
 const postroute_1 = __importDefault(require("./routes/postroute"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const http_1 = __importDefault(require("http"));
+const path_1 = __importDefault(require("path"));
 dotenv_1.default.config();
+const newesetversion = "1.0.3";
 const app = (0, express_1.default)();
 const server = http_1.default.createServer(app);
 const PORT = process.env.PORT || 3080;
 app.use(express_1.default.json({ limit: "200mb" }));
+console.log(path_1.default.join(__dirname, "public", "html"));
 app.use((0, cors_1.default)({ origin: "*" }));
 app.use((0, morgan_1.default)("dev"));
 app.use("/api/users", userroute_1.default);
 app.use("/api/posts", postroute_1.default);
+app.use(express_1.default.static(path_1.default.join(process.cwd(), "public")));
+app.get("/", (req, res) => {
+    res.sendFile(path_1.default.join(process.cwd(), "public", "home.html"));
+});
+app.get("/eula", (req, res) => {
+    res.sendFile(path_1.default.join(process.cwd(), "public", "eula.html"));
+});
+app.get("/privacy", (req, res) => {
+    res.sendFile(path_1.default.join(process.cwd(), "public", "privacy.html"));
+});
+app.get("/support", (req, res) => {
+    res.sendFile(path_1.default.join(process.cwd(), "public", "support.html"));
+});
+app.get("/version", (req, res) => {
+    res.json({ version: newesetversion });
+});
 server.listen(PORT, () => {
     console.log("listening on port", PORT);
 });
--- src/server.ts
+++ src/server.ts
@@ -5,18 +5,43 @@ import userRoute from "./routes/userroute";
 import postRoute from "./routes/postroute";
 import dotenv from "dotenv";
 import http from "http";
+import path from "path";
 dotenv.config();
 
+const newesetversion = "1.0.3";
+
 const app = express();
+
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3080;
 app.use(express.json({ limit: "200mb" }));
+console.log(path.join(__dirname, "public", "html"));
 
 app.use(cors({ origin: "*" }));
 app.use(morgan("dev"));
 
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
+app.use(express.static(path.join(process.cwd(), "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "public", "home.html"));
+});
+app.get("/eula", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "public", "eula.html"));
+});
+
+app.get("/privacy", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "public", "privacy.html"));
+});
+
+app.get("/support", (req, res) => {
+  res.sendFile(path.join(process.cwd(), "public", "support.html"));
+});
+
+app.get("/version", (req, res) => {
+  res.json({ version: newesetversion });
+});
 
 server.listen(PORT, () => {
   console.log("listening on port", PORT);
